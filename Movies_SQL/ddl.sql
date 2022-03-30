76473e861451023d48dd661ccce12456a6a5f6de
nika7724 <nika0147@stud.kea.dk>
2022-03-30 22:53:40 +0200

    First push

---
+++ Movies_SQL/ddl.sql
@@ -0,0 +1,22 @@
+SHOW TABLES;
+#create table movies, id, title
+
+#create table director
+
+#create table actor
+
+#create table movies and acotrs
+
+#create production_teams table
+
+#Add column year
+
+#Delete column year
+
+#Modify year
+
+#delete table movies
+
+#Databases, Tables, Columns vs. Data(in rows)
+#DDL vs. DML
+#CRUD
---
+++ Movies_SQL/dml_basic.sql
@@ -0,0 +1,30 @@
+#CRUD
+###################
+#READ
+###################
+
+#Read this as read all entries (rows) from the table movies
+#*is a wild card for ALL
+
+
+
+
+####################
+#Create
+###################
+#insert data into title
+#"Don't look up"
+#"Interstellar"
+
+####################
+#Update
+####################
+#Update King Kong to be King Kong vs. Godzilla
+
+####################
+#Delete
+####################
+#delete title "Don't look up";
+#delete tiitle "Don't look up"  LIMIT 5;
+
+
