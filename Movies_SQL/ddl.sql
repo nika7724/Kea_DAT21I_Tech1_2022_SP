719a986bc71b001a58aa897242d8d0a0562def41
nika7724 <nika0147@stud.kea.dk>
2022-03-31 15:26:44 +0200

    First push

--- Movies_SQL/ddl.sql
+++ Movies_SQL/ddl.sql
@@ -1,21 +1,44 @@
 SHOW TABLES;
+CREATE DATABASE movies;
+USE movies;
 #create table movies, id, title, Define a primary key in movies
-
+CREATE TABLE IF NOT EXISTS movies(
+id int AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL
+);
 #create table director, #Create a foreign key on direcotors that points to movies
-
+CREATE TABLE IF NOT EXISTS directors(
+    id int AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),
+movie_id int,
+FOREIGN KEY(movie_id) REFERENCES movies(id)
+);
 #create table actor
-
+CREATE TABLE IF NOT EXISTS actors (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
 #create table movies and acotrs
+CREATE TABLE IF NOT EXISTS movies_actors (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT,
+    actor_id INT,
+    FOREIGN KEY (movie_id) REFERENCES movies(id),
+    FOREIGN KEY (actor_id) REFERENCES actors(id)
+);
 
 #create production_teams table
+CREATE TABLE IF NOT EXISTS production_teams (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
 
 #Add column year
-
+ALTER TABLE movies ADD COLUMN year YEAR;
 #Delete column year
-
+ALTER TABLE movies DROP COLUMN year;
 #Modify year
-
+ALTER TABLE movies MODIFY year INT(4);
 #delete table movies
+DROP TABLE movies;
 
 #Databases, Tables, Columns vs. Data(in rows)
 #DDL vs. DML
--- Movies_SQL/dml_basic.sql
+++ Movies_SQL/dml_basic.sql
@@ -5,9 +5,7 @@
 
 #Read this as read all entries (rows) from the table movies
 #*is a wild card for ALL
-
-
-
+SELECT* FROM movies;
 
 ####################
 #Create
@@ -15,16 +13,23 @@
 #insert data into title
 #"Don't look up"
 #"Interstellar"
+INSERT INTO movies (title) VALUES ('Don''t look up');
+INSERT INTO movies (title) VALUES ('Interstellar');
+INSERT INTO movies (title) VALUES('King Kong');
+
 
 ####################
 #Update
 ####################
+#UPDATE title 'Don''t look up to 'Pretty Woman'
+UPDATE movies SET title = 'Pretty Woman' WHERE id= 2;
 #Update King Kong to be King Kong vs. Godzilla
-
+UPDATE movies SET title ='King Kong vs. Godzilla' WHERE id = 4;
 ####################
 #Delete
 ####################
 #delete title "Don't look up";
+DELETE FROM movies WHERE title;
 #delete tiitle "Don't look up"  LIMIT 5;
-
+DELETE FROM movies WHERE title = 'Don''t look up' LIMIT 5;
 
